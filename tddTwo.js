5599c8c1c001ff998e4e2877da3541614b810af3
Andy Gavin <andrew.gavin17@gmail.com>
2018-02-06 14:53:13 -0700

    we tried really hard to make this work but to mixed success

--- tddTwo.js
+++ tddTwo.js
@@ -1,1 +1,37 @@
-//Using TDD, (wherein you write tests BEFORE your function), write a function that logs every element in an array of integers.
+//Using TDD, (wherein you write tests BEFORE your function), write a function that returns an array of the first and last element in an array of integers.
+var emptyArray = [];
+
+
+function array(notN){ 
+    emptyArray.push(notN[0]);
+    emptyArray.push(notN[notN.length - 1]); 
+    return emptyArray;
+}
+
+
+
+if (array([1,2,10])[0] == 1){
+    console.log("pass1");
+
+}
+else {
+    console.log("fail1");
+}
+if (array([1,2,3])[2] == 3) {
+    console.log("pass2");
+} else {
+    console.log("fail2");
+}
+
+if (array([1,4,10])[1] == 4) {
+    console.log("pass3");
+} else {
+    console.log("fail3");
+}
+
+if (array([1,2,4,5,6]) == [1,6]) {
+    console.log("PASS4");
+} else {
+    console.log("FAIL4");
+}
+//console.log(array("123"[2]));
